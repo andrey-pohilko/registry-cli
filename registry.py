15f0c00252febc86e3c7fbc20c2910907b69f92c
Tarek M. Sayed <24387820+TarekMSayed@users.noreply.github.com>
2025-08-21 10:46:50 +0300

    fix: registry-2.8.2 by loop on pages (#121)
    LGTM!

--- registry.py
+++ registry.py
@@ -264,11 +264,20 @@ class Registry:
         return None
 
     def list_images(self):
-        result = self.send('/v2/_catalog?n=10000')
-        if result is None:
-            return []
+        images = []
+        last = ""
+        # loop through all pages and get 10 records every time
+        while True:
+            result = self.send('/v2/_catalog?n=10&last=' + last)
+            if result is None:
+                return images
+            repos = json.loads(result.text)['repositories']
+            if len(repos) == 0:
+                break
+            images += repos
+            last = repos[-1]
 
-        return json.loads(result.text)['repositories']
+        return images
 
     def list_tags(self, image_name):
         result = self.send("/v2/{0}/tags/list".format(image_name))
